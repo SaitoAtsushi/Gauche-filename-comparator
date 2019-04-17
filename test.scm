0caa81349000882e647209dd2f82000c15ab345b
SAITO Atsushi <maldikulo@gmail.com>
2019-04-17 13:49:49 +0900

    Fix for case of zero padding case.

--- test.scm
+++ test.scm
@@ -10,6 +10,7 @@
 (test-module 'filename-comparator)
 
 (test* "less" #t (<? filename-comparator "abc40" "abc300"))
+(test* "less" #t (<? filename-comparator "abc040" "abc40"))
 (test* "not less" #f (<? filename-comparator "abc40" "abc31"))
 (test* "equal" #t (=? filename-comparator "abc30" "abc30"))
 (test* "not equal" #f (=? filename-comparator "abc30" "cba30"))
